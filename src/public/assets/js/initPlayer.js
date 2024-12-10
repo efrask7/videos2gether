ee754a84fb2e992f100cde9815de596ed7e538bc
efrask7 <efrainschettini.123@gmail.com>
2024-12-09 21:20:32 -0300

    Se cambio el IFrame por el api de embed de yt

---
+++ src/public/assets/js/initPlayer.js
@@ -0,0 +1,51 @@
+const tag = document.createElement('videoyt_player');
+
+tag.src = "https://www.youtube.com/iframe_api";
+const firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+let player;
+
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('video_player', {
+    height: '100%',
+    width: '100%',
+    videoId: 'KLuTLF3x9sA',
+    playerVars: {
+      'playsinline': 1,
+      'enablejsapi': 1,
+      'origin': window.location.origin,
+      'autoplay': 1,
+      'controls': 0,
+      'mute': 1,
+    },
+    events: {
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
+    }
+  });
+}
+
+function onPlayerReady(event) {
+  // El video está listo para reproducirse
+  console.log('Player ready');
+}
+
+let done = false;
+
+function onPlayerStateChange(event) {
+  // Manejar cambios de estado
+  console.log('Player state changed:', event.data);
+  if (event.data == YT.PlayerState.PLAYING && !done) {
+    setTimeout(stopVideo, 6000);
+    done = true;
+  }
+}
+
+function stopVideo() {
+  console.log('Stop video')
+  player.stopVideo();
+}
+
+// Exportar el player para usarlo en otros archivos
+window.player = player;
